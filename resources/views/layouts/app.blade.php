d6b1db3ce8e2048af3a26fe1949488178c60655a
Henrique Luis <frostsalazar@hotmail.com>
2025-05-22 21:41:05 -0300

    Atualizado
    CSS Atualizado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-    <title>@yield('titulo', 'minha aplicaçao')</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('titulo', 'Minha Aplicação')</title>
+    <link rel="stylesheet" href="{{ asset('css/geral.css') }}">
 </head>
 <body>
     <div class="container">
